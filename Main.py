a888ce488812ffd3ac977910e2c0e12e187e5dde
Renan <renanjochem98@gmail.com>
2020-03-13 14:37:16 -0300

    Adicionando entradas e saidas no terminal para ajudar a manusear o algoritmo

--- Main.py
+++ Main.py
@@ -1,5 +1,33 @@
 from Graph import Graph
 
 g = Graph()
-g.ler("D:\\Users\\flavi_000\\Documents\\Sistemas de Informação\\2020.1\\INE5413 - Grafos\\instancias\\caminho_minimo\\fln_pequena.net")
+g.ler("fln_pequena.net")
+user_input = -1
+while user_input != 0:
+    print("Qual operação você deseja realizar:")
+    print("1 - Ver a quantidade de Vertices")
+    print("2 - Ver a quantidade de Arestas")
+    print("0 - Finalizar o algoritmo")
+    try:
+        user_input = int(input("Digite a opção desejada:"))
+    except:
+        print()
+        print("Você não digitou uma opção válida")
+        print()
+        continue
 
+    if user_input == 1:
+        qtdVertex = g.qtdVertices()
+        print()
+        print("O grafo tem "+str(qtdVertex)+" vertices.")
+        print()
+    if user_input == 2:
+        qtdArestas = g.qtdArestas()
+        print()
+        print("O grafo tem "+str(qtdArestas)+" arestas.")
+        print()
+    if user_input >= 3:
+        user_input = 0
+
+print()
+print("Algoritmo finalizado")
