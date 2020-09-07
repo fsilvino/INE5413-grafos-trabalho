466b81fdfa1a0909d23af2ad171c356193661c25
Renan <renanjochem98@gmail.com>
2020-09-07 11:38:26 -0300

    Alterando forma de exibir as saidas da tela.

--- Main.py
+++ Main.py
@@ -2,46 +2,56 @@ from Graph import Graph
 
 g = Graph()
 g.ler("fln_pequena.net")
-# g.show_graph()
-# v = g.busca_largura(8, 1)
-# g.show_result_busca_largura(v)
-# print(v[1])
-#     print(v.relationships)
-# print(g.vertices)
+
+def mostrarQtdVertices():
+    qtdVertex = g.qtdVertices()
+    print()
+    print("O grafo tem "+str(qtdVertex)+" vertices.")
+
+def mostrarQtdArestas():
+    qtdArestas = g.qtdArestas()
+    print()
+    print("O grafo tem "+str(qtdArestas)+" arestas.")
+
+def buscaLargura():
+    buscado = int(input("Digite o valor buscado:"))
+    inicio = int(input("Digite o numero do vertice inicial:"))
+    result = g.busca_largura(inicio, buscado)
+    print("O resultado da busca em largura será mostrado abaixo:")
+    g.show_result_busca_largura(result)
+
+def mostrarGrafo():
+    g.show_graph()
+
+# lista com funcoes que serao execudatas
+acoes = [
+    mostrarGrafo,
+    mostrarQtdVertices,
+    mostrarQtdArestas,
+    buscaLargura
+]
+
 user_input = -1
 while user_input != 0:
     print("Qual operação você deseja realizar:")
-    print("1 - Ver a quantidade de Vertices")
-    print("2 - Ver a quantidade de Arestas")
-    print("3 - Realizar busca em largura")
+    print("1 - Mostrar grafo")
+    print("2 - Ver a quantidade de Vertices")
+    print("3 - Ver a quantidade de Arestas")
+    print("4 - Realizar busca em largura")
     print("0 - Finalizar o algoritmo")
     try:
         user_input = int(input("Digite a opção desejada:"))
+        if user_input > 0 and user_input <= len(acoes):
+            acoes[user_input - 1]()
+            print()
+        else:
+            user_input = 0
     except:
         print()
         print("Você não digitou uma opção válida")
         print()
         continue
 
-    if user_input == 1:
-        qtdVertex = g.qtdVertices()
-        print()
-        print("O grafo tem "+str(qtdVertex)+" vertices.")
-        print()
-    if user_input == 2:
-        qtdArestas = g.qtdArestas()
-        print()
-        print("O grafo tem "+str(qtdArestas)+" arestas.")
-        print()
-    if user_input == 3:
-        buscado = int(input("Digite o valor buscado:"))
-        inicio = int(input("Digite o numero do vertice inicial:"))
-        result = g.busca_largura(inicio, buscado)
-        print("O resultado da busca em largura será mostrado abaixo:")
-        g.show_result_busca_largura(result)
-        print()
-    if user_input >= 4:
-        user_input = 0
 
 print()
 print("Algoritmo finalizado")
